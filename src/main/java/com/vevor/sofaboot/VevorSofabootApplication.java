26a4bd4008e4af39d4cb069834999d555bf281f9
root <admin@example.com>
2019-09-20 17:33:36 +0800

    Initial commit

---
+++ src/main/java/com/vevor/sofaboot/VevorSofabootApplication.java
@@ -0,0 +1,13 @@
+package com.vevor.sofaboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VevorSofabootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VevorSofabootApplication.class, args);
+    }
+
+}
