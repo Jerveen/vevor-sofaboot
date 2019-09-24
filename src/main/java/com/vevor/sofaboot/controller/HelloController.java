95b0c54039c1780f22b57fababb280b8490e2cfe
Jerveen <17611211005@163.com>
2019-09-24 19:15:58 +0800

    1.优化

--- src/main/java/com/vevor/sofaboot/controller/HelloController.java
+++ src/main/java/com/vevor/sofaboot/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.vevor.sofaboot.controller;
 
+import com.alipay.sofa.runtime.api.annotation.SofaReference;
+import com.alipay.sofa.runtime.api.annotation.SofaReferenceBinding;
+import com.vevor.sofaboot.sevice.HelloService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+
+    @SofaReference(interfaceType = HelloService.class, binding = @SofaReferenceBinding(bindingType = "bolt"))
+    private HelloService helloService;
+
+    @GetMapping("/sayHello")
+    public String sayHello(String name) {
+
+        String result = helloService.sayHello(name);
+
+        return result;
+    }
 }
